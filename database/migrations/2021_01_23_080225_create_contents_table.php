44833553c324675be2286d75de7d0470463f37ad
Rabinson <myself.ronin@hotmail.com>
2021-01-23 13:59:11 +0545

    initial flow builder migrations, models and controllers

---
+++ database/migrations/2021_01_23_080225_create_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('message_id');
+            $table->string('content_type');
+            $table->bigInteger('content_id');
+            $table->integer('index');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
---
+++ database/migrations/2021_01_23_080514_create_buttons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateButtonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buttons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buttons');
+    }
+}
